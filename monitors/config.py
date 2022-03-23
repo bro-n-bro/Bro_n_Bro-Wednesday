6cb183035412dc5a3a7318f75c26175ca1d08013
bro-n-bro-0 <89854952+bro-n-bro-0@users.noreply.github.com>
2022-03-23 18:18:17 +0100

    Update config.py

--- monitors/config.py
+++ monitors/config.py
@@ -21,7 +21,6 @@ BLACK_LIST = [
     'osmo14nzyt8wmx4g6zkeluelukamgsh5v4xgnnzn4j8',
     'osmo1cg79pj70mgl8xlum0rw5yy6enk9jszsrfyf8cx',
     'osmo1tqdvlku97p88z5ndf3jz55vnccmzgs8jla375r',
-    'osmo1s2a7rckcky6jmhncxwy0xtwf2ymg87puaj4z2h',
     'osmo1pvxhtre74l37p6y2rs2e8xyek75z7xlc7g2trt',
     'emoney1cpfn66xumevrx755m4qxgezw9j5s86qkan5ch8',
     'stars139a4n6w6dhwv60dj2clgwm6r0q84gju28z9at0',
